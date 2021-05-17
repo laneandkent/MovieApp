e63534796713562798645374f6e2afa7aec9602e
Missy <82602150+laneandkent@users.noreply.github.com>
2021-05-17 13:11:43 -0500

    added Wayne's World; finally got centering to work

--- my-movie-app/src/app/app.component.ts
+++ my-movie-app/src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from './movie.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-movie-app';
+  selectedMovie = Movie;
   loadedList = 'current';
   onNavigate(film: string){
     this.loadedList = film;
   }
+  ngOnInit(): void {
+  }
+
 }
--- my-movie-app/src/app/movies-list/movies-list.component.ts
+++ my-movie-app/src/app/movies-list/movies-list.component.ts
@@ -11,7 +11,8 @@ export class MoviesListComponent implements OnInit {
   movies: Movie[] = [
     new Movie('Shrek', '2001', 'https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY1200_CR88,0,630,1200_AL_.jpg', 'DVD', true, 5, 'https://www.imdb.com/title/tt0126029/?ref_=fn_al_tt_1'),
     new Movie('Moonstruck', '1987', 'https://m.media-amazon.com/images/M/MV5BMjIwMDY0NzYyMF5BMl5BanBnXkFtZTcwOTE5NDk0NA@@._V1_UY1200_CR85,0,630,1200_AL_.jpg', 'DVD', true, 5, 'https://www.imdb.com/title/tt0093565/?ref_=fn_al_tt_1'),
-    new Movie('Superman', '1978', 'https://i.ebayimg.com/images/g/r9EAAOSwH2VaMdEF/s-l300.jpg', 'DVD', true, 5, 'https://www.imdb.com/title/tt0078346/?ref_=fn_al_tt_8')
+    new Movie('Superman', '1978', 'https://i.ebayimg.com/images/g/r9EAAOSwH2VaMdEF/s-l300.jpg', 'DVD', true, 5, 'https://www.imdb.com/title/tt0078346/?ref_=fn_al_tt_8'),
+    new Movie('Wayne\'s World', '1992', 'https://i.ytimg.com/vi/OyXlX124mgw/movieposter.jpg', 'Digital', true, 5, 'https://www.imdb.com/title/tt0105793/?ref_=fn_al_tt_1')
   ];
 
   constructor() { }
